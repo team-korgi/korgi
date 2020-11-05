51034019915740b99dbbc6800f1d14759e111936
Clemens <clemens.jurkowitsch@gmx.at>
2020-11-05 11:15:33 +0100

    Added Favicon

---
+++ database/migrations/2020_10_29_150724_create_pubnub_keys.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePubnubKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pubnub_keys', function (Blueprint $table) {
+            $table->id();
+            $table->string('subscribe_key');
+            $table->string('publish_key');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pubnub_keys');
+    }
+}
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+
         <title>{{ config('app.name', 'Korgi') }}</title>
 
         <!-- Fonts -->
